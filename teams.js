cfd3b4dfea13dd01bc0a5945747e65bbdfbfed77
David Monroy <david2310.dmc@gmail.com>
2022-07-30 06:44:28 +0100

    Includes teams and the Teams class

---
+++ teams.js
@@ -0,0 +1,18 @@
+export const teams = [
+  "Inglaterra",
+  "Paises Bajos",
+  "Dinamarca",
+  "Noruega",
+  "España",
+  "Finlandia",
+  "Suecia",
+  "Francia",
+  "Belgica",
+  "Alemania",
+  "Italia",
+  "Islandia",
+  "Austria",
+  "Portugal",
+  "Suiza",
+  "Irlanda del Norte",
+];
