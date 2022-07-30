b35cbdee6587a859b2a9545885271e6b3950c725
David Monroy <david2310.dmc@gmail.com>
2022-07-30 06:48:35 +0100

    Includes the League class, the index and the packageJSON, tested the league

---
+++ index.js
@@ -0,0 +1,10 @@
+import { League } from "./classes/League.js"
+import { Team } from "./classes/Team.js"
+import { teams as teamNames } from "./teams.js"
+
+const liga = new League("Eurocopa Femenina 2022")
+const teams = teamNames.map((team) => {
+  return new Team(team)
+})
+
+liga.startLeague(teams)
